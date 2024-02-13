ec8288a7e751f47a242b190d0d2d7f74ff4c06a6
Arindam Saha <arindamsaha1507@gmail.com>
2024-02-13 17:24:40 +0000

    Separated Components

---
+++ emulode/components.py
@@ -0,0 +1,178 @@
+"""Module for the components of the project"""
+
+from dataclasses import dataclass, field
+
+import numpy as np
+from emulode.config import Configs
+from emulode.emulator import Emulator
+
+from emulode.ode import ODE
+from emulode.plotter import Plotter
+from emulode.simulator import Simulator
+from emulode.solver import Solver
+
+
+@dataclass
+class ODEFactory:
+    """Factory class for the ODE function."""
+
+    config: Configs
+
+    function: callable = field(init=False)
+    parameters: dict = field(init=False)
+
+    def __post_init__(self) -> None:
+
+        self.function = self.ode_function_chooser(self.config.ode.chosen_ode)
+        self.parameters = self.config.ode.parameters
+
+    @staticmethod
+    def ode_function_chooser(chosen_ode: str) -> callable:
+        """Choose the ODE function based on the given string."""
+        if chosen_ode == "Rossler":
+            return ODE.rossler
+        if chosen_ode == "Lorenz":
+            return ODE.lorenz
+        raise ValueError(f"ODE '{chosen_ode}' not found")
+
+
+@dataclass
+class SolverFactory:
+    """Factory class for the solver."""
+
+    ode_factory: ODEFactory
+    configs: Configs
+
+    initial_conditions: list[float] = field(init=False)
+    t_range: tuple[float, float] = field(init=False)
+    n_steps: int = field(init=False)
+    transience: int = field(init=False)
+
+    solver: Solver = field(init=False)
+
+    def __post_init__(self) -> None:
+
+        self.initial_conditions = self.configs.solver.initial_conditions
+        self.t_range = tuple(self.configs.solver.t_range)
+        self.n_steps = self.configs.solver.n_steps
+        self.transience = self.configs.solver.transience
+
+        self.solver = Solver(
+            self.ode_factory.function,
+            self.ode_factory.parameters,
+            self.initial_conditions,
+            self.t_range,
+            self.n_steps,
+            self.transience,
+        )
+
+
+@dataclass
+class SimulatorFactory:
+    """Factory class for the simulator."""
+
+    solver_factory: SolverFactory
+    configs: Configs
+
+    parameter_of_interest: str = field(init=False)
+    range_of_interest: tuple[float, float] = field(init=False)
+    n_points: int = field(init=False)
+    component_of_interest: int = field(init=False)
+
+    simulator: Simulator = field(init=False)
+
+    def __post_init__(self) -> None:
+
+        self.parameter_of_interest = self.configs.simulator.parameter_of_interest
+        self.range_of_interest = tuple(self.configs.simulator.range)
+        self.n_points = self.configs.simulator.n_simulation_points
+        self.component_of_interest = self.configs.simulator.component_of_interest
+
+        self.simulator = Simulator(
+            self.solver_factory.solver,
+            self.parameter_of_interest,
+            self.range_of_interest[0],
+            self.range_of_interest[1],
+            self.n_points,
+            component_of_interest=self.component_of_interest,
+        )
+
+
+@dataclass
+class EmulatorFactory:
+    """Factory class for the emulator."""
+
+    simulator_factory: SimulatorFactory
+    configs: Configs
+
+    n_layers: int = field(init=False)
+    n_predict: int = field(init=False)
+    n_iterations: int = field(init=False)
+
+    emulator: Emulator = field(init=False)
+
+    def __post_init__(self) -> None:
+
+        self.n_layers = self.configs.emulator.n_layers
+        self.n_predict = self.configs.emulator.n_prediction_points
+        self.n_iterations = self.configs.emulator.n_iterations
+
+        self.emulator = Emulator(
+            self.simulator_factory.simulator.xdata,
+            self.simulator_factory.simulator.ydata,
+            self.n_layers,
+            self.n_predict,
+            self.n_iterations,
+        )
+
+
+@dataclass
+class PlotterFactory:
+    """Factory class for the plotter."""
+
+    # pylint: disable=too-many-arguments
+
+    configs: Configs
+    solver: Solver
+    simulator: Simulator
+    emulator: Emulator
+
+    directory: str = field(init=False)
+    filename: str = field(init=False)
+    xlabel: str = field(init=False)
+    ylabel: str = field(init=False)
+    scale: float = field(init=False)
+    x_ideal: np.ndarray = field(init=False)
+    y_ideal: np.ndarray = field(init=False)
+
+    plotter: Plotter = field(init=False)
+
+    def __post_init__(self) -> None:
+
+        self.directory = self.configs.plotter.directory
+        self.filename = self.configs.plotter.filename
+        self.xlabel = self.configs.plotter.x_label
+        self.ylabel = self.configs.plotter.y_label
+
+        self.scale = (self.configs.solver.t_final - self.configs.solver.t_initial) * (
+            1 - self.configs.solver.transience
+        )
+
+        self.x_ideal = np.linspace(0, self.scale, len(self.solver.results[0, :]))
+        self.y_ideal = self.solver.results[
+            self.configs.simulator.component_of_interest, :
+        ]
+
+        Plotter.create_combined_plot(
+            f"{self.directory}/{self.filename}",
+            self.xlabel,
+            self.ylabel,
+            self.simulator.xdata,
+            self.simulator.ydata,
+            self.emulator.x_predict,
+            self.emulator.y_predict,
+            self.emulator.y_var,
+            scale=self.scale,
+            x_ideal=self.x_ideal,
+            y_ideal=self.y_ideal,
+        )
--- run.py
+++ run.py
@@ -2,16 +2,16 @@
 
 import os
 
-import yaml
+# import numpy as np
 
-import numpy as np
-
-from emulode.solver import Solver
-from emulode.emulator import Emulator
-from emulode.ode import ODE
-from emulode.plotter import Plotter
-from emulode.simulator import Simulator
 from emulode.config import Configs
+from emulode.components import (
+    ODEFactory,
+    SolverFactory,
+    SimulatorFactory,
+    EmulatorFactory,
+    PlotterFactory,
+)
 
 
 def trial() -> None:
@@ -25,77 +25,19 @@ def main() -> None:
 
     # pylint: disable=too-many-locals
 
-    with open("config.yml", "r", encoding="utf-8") as file:
-        config = yaml.safe_load(file)
+    configs = Configs(os.path.join(os.getcwd(), "config.yml"))
 
-    chosen_ode = config["ode"]["chosen_ode"]
-
-    if chosen_ode == "Rossler":
-        ode_func = ODE.rossler
-    elif chosen_ode == "Lorenz":
-        ode_func = ODE.lorenz
-    else:
-        raise ValueError(f"ODE '{chosen_ode}' not found")
-
-    params = config["ode"]["parameters"][chosen_ode]
-    init_conds = np.array(config["solver"]["initial_conditions"])
-    t_span = tuple(config["solver"]["t_range"])
-    t_steps = config["solver"]["n_steps"]
-    transience = config["solver"]["transience"]
-
-    solver = Solver(ode_func, params, init_conds, t_span, t_steps, transience)
-    print(solver)
-
-    parameter_of_interest = config["simulator"]["parameter of interest"]
-    component_of_interest = config["simulator"]["component of interest"]
-    range_of_interest = tuple(config["simulator"]["range"])
-    n_points = config["simulator"]["n_simulation_points"]
-
-    simulator = Simulator(
-        solver,
-        parameter_of_interest,
-        range_of_interest[0],
-        range_of_interest[1],
-        n_points,
-        component_of_interest=component_of_interest,
-    )
-
-    # simulator = Simulator(solver, "c", 5, 10, 10, lambda x: np.max(x[0, :]))
-
-    print(simulator)
-
-    n_layers = config["emulator"]["n_layers"]
-    n_predict = config["emulator"]["n_prediction_points"]
-    n_iterations = config["emulator"]["n_iterations"]
-
-    emulator = Emulator(
-        simulator.xdata, simulator.ydata, n_layers, n_predict, n_iterations
-    )
-
-    print(emulator)
-
-    directory = config["plotter"]["directory"]
-    filename = config["plotter"]["filename"]
-    xlabel = config["plotter"]["x_label"]
-    ylabel = config["plotter"]["y_label"]
-
-    scale = (t_span[1] - t_span[0]) * (1 - transience)
-
-    Plotter.create_combined_plot(
-        f"{directory}/{filename}",
-        xlabel,
-        ylabel,
-        simulator.xdata,
-        simulator.ydata,
-        emulator.x_predict,
-        emulator.y_predict,
-        emulator.y_var,
-        scale=scale,
-        x_ideal=np.linspace(0, scale, len(solver.results[0, :])),
-        y_ideal=solver.results[component_of_interest, :],
+    ode = ODEFactory(configs)
+    solver = SolverFactory(ode, configs)
+    simulator = SimulatorFactory(solver, configs)
+    emulator = EmulatorFactory(simulator, configs)
+    PlotterFactory(
+        configs,
+        solver.solver,
+        simulator.simulator,
+        emulator.emulator,
     )
 
 
 if __name__ == "__main__":
-    # main()
-    trial()
+    main()
